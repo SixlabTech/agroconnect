1c8021404f715d3e0c1995878ed3e29e3aca9a93
josephbasix <josephbasix@gmail.com>
2024-10-07 21:14:50 +0100

    Render BannerHeroSection component responsive and align elements horizontally

--- src/components/heroSection/BannerHeroSction.jsx
+++ src/components/heroSection/BannerHeroSction.jsx
@@ -25,11 +25,11 @@ const items = [
 
 export default function BannerHeroSection() {
   return (
-    <div className="flex flex-col gap-4 p-2 md:p-4  rounded-sm justify-center items-center">
+    <div className="flex flex-col border md:flex-row md:gap-20 lg:gap-20 p-2 md:p-4 rounded-sm justify-center items-center">
       {items.map((item, index) => (
-        <div key={index} className="flex flex-col items-center text-center gap-2 md:gap-4 w-full md:w-auto">
+        <div key={index} className="flex flex-col justify-center items-center text-center gap-2 md:gap-4 w-full md:w-auto">
           <img src={item.imgUrl} alt="SVG" className="w-6 h-6 md:w-7 md:h-7" />
-          <div className="flex flex-col items-center">
+          <div className="flex flex-col justify-center   items-center ">
             <p className="font-medium text-xs md:text-base">{item.title}</p>
             <p className="text-text-gray font-thin text-xs md:text-sm">{item.description}</p>
           </div>
